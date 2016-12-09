fa8f5fbf566577d9eaf72093dfabdea7fb2df3cb
Khasim Peera Durgam <Khasim Peera Durgam>
2016-12-10 00:18:05 +0530

    first commit

---
+++ src/test/java/com/khasim/AppTest.java
@@ -0,0 +1,26 @@
+package com.khasim;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+	
+	@Test(priority=0)
+	public void loginTest()
+	{
+		System.out.println("login");
+	}
+	
+	@Test(priority=5)
+	public void registrationTest()
+	{
+		System.out.println("registration");
+	}
+	
+	@Test(priority=2)
+	public void logoutTests()
+	{
+		System.out.println("logout");
+	}
+	
+
+}
